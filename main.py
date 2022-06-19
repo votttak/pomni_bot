830492d23cfaf55001839005e229d330afa5846f
Vadim <pochali8778sdvig@gmail.com>
2022-06-19 11:52:20 +0200

    19062022

--- main.py
+++ main.py
@@ -3,130 +3,65 @@ import telebot
 
 bot = telebot.TeleBot('5540979383:AAHjJMwpp4Jlb6NhfwO1CqkGG23gCvjMob0')
 
-# @bot.message_handler(commands=['start'])
-# def start(message):
-#     if message.text == 'start':
-#         bot.send_message(message.from_user.id, "привет", parse_mode=None)
-#         bot.send_message(message.from_user.id, "<b>привет</b>", parse_mode='html')                 # message.chat.id <-- через сообщение мы обратились к чату и получили его индентификатор
-#     else:
-#         bot.send_message(message.from_user.id, "Напиши /start")
-
-
-@bot.message_handler(content_types=['text'])
-def start_2(message):
-    print(message)
-    # if message.text == 'start':
-    #     bot.send_message(message.from_user.id, "привет", parse_mode=None)
-    #     bot.send_message(message.from_user.id, "<b>привет</b>", parse_mode='html')                 # message.chat.id <-- через сообщение мы обратились к чату и получили его индентификатор
-    # else:
-    #     bot.send_message(message.from_user.id, "Напиши /start")
-
-# @bot.channel_post_handler()
-# def start_2(message):
-#     print("HERE")
-#     print(message)   
-#     print(message.chat.id) 
-#     bot.forward_message(468918244, message.chat.id, 300)  
-    
-
-
-##################################################################
-
 import threading
 import time
 import random
- 
 
+chats_dict = {} # chat_id : (Thread, ChatContext)
 
-chats_dict = {} 
-
-# chats_dict =  { chat_id_1: {"thread_id": thread_id_1; "messages_id": []},
-#                 chat_id_2: {"thread_id": thread_id_2; "messages_id": []}            
-#               }
-
-
-
-
-last_message_id = 0
-last_task = None
-last_chat_id = 0
-stop_thread = None
+class ChatContext:
+    def __init__(self, chat_id, last_message_id):
+        self.messages_ids = list(range(last_message_id))
+        self.chat_id = chat_id
+        
+    def handle_new_message(self, message_id):
+        self.messages_ids.append(message_id)
 
 
 
 
 
-
- 
 @bot.channel_post_handler()
 def handle_chat_message(message):
     
-    global last_message_id
-    global last_chat_id
-    global last_task
-    global chats_dict
-
-    last_message_id = message.id
-    last_chat_id = message.chat.id
-    
-    if last_chat_id in chats_dict: 
-        old_thread = chats_dict[last_chat_id]["thread_id"]
-
-        global stop_thread 
-        stop_thread = True
-        old_thread.join()
-
-        # chats_dict[last_chat_id]["message_ids"] = list(range(last_message_id))
-        # chats_dict[last_chat_id]["thread_id"] = threading.Thread(target=random_forward_task(last_chat_id, last_message_id))
-        # chats_dict[last_chat_id]["thread_id"].start()
-        
-        
+    if message.chat.id in chats_dict:
+        print("Чат обновился")
+        chat_thread, chat_context = chats_dict[message.chat.id]
+        chat_context.handle_new_message(message.id)
     else:
-        stop_thread = False
-        last_task = threading.Thread(target=random_forward_task(last_chat_id, last_message_id))
-        chats_dict[last_chat_id] = {"message_ids": list(range(last_message_id)), "thread_id": last_task}
-        last_task.start()
+        print("Новый чат начал повторение")
+        chat_context = ChatContext(message.chat.id, message.id)
+        chat_thread = threading.Thread(target=chat_thread_logic, args=(chat_context,))
+        chat_thread.start()
+        chats_dict[message.chat.id] = (chat_thread, chat_context)
 
-
-
-
-    
-def random_forward_task(chat_id, last_msg_id_local):
-
-    global stop_thread
-
+def chat_thread_logic(chat_context):
     while True:
-        time.sleep(5)
-        rnd_msg_id = random.randint(0, last_msg_id_local)
+
+        seconds_in_week = 604800
+        seconds_in_day = 24*60*60
+        seconds_in_two_h = 2*60*60
+        print("Спим " + str(seconds_in_two_h) + "секунд")
+        time.sleep(seconds_in_two_h)
         
-        print("LAST_MESSAGE_ID")
-        print(last_msg_id_local)
+        successfully_forwaded = False
         
-        isSuccess = False
+        while successfully_forwaded == False:
+            rand_message_id = random.choice(chat_context.messages_ids)
+            
+            successfully_forwaded = try_forward_message_id(chat_context.chat_id, rand_message_id)
+            
+            if successfully_forwaded == False:
+                chat_context.messages_ids.remove(rand_message_id)
+                time.sleep(2)
         
-        if stop_thread:
-            break
-        
-        while isSuccess == False:
-            try:
-                # print(1)
-                bot.forward_message(468918244, chat_id, rnd_msg_id)
-                # print(3)
-                isSuccess = True
-            except:
-                # print(2)
-                isSuccess = True
-    
-    # stop_thread = False
- 
+def try_forward_message_id(chat_id, message_id):
+    try:
+        bot.forward_message(468918244, chat_id, message_id)
+        return True
+    except:
+        return False
 
-
-
-##################################################################
-
-
-
-
-
-
-bot.polling(none_stop=True)
+# bot.polling(none_stop=True)
+# bot.infinity_polling(True)
+bot.infinity_polling(timeout=10, long_polling_timeout = 5)
